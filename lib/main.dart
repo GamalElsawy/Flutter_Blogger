1c0b7b781f09f0130275fa4c41b882c7ce693cc7
Gamal Elsawy <gamalelsawy11@gmail.com>
2020-06-11 07:02:21 +0200

    New Edit

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter New App',
       theme: ThemeData(
         // This is the theme of your application.
         //
